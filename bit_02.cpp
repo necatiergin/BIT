3fa73a94ec5d932026b1b5e014208e175ec197ed
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-04 18:02:06 +0300

    Create bit_02.cpp

---
+++ bit_02.cpp
@@ -0,0 +1,12 @@
+#include <bit>
+#include <bitset>
+
+
+int main()
+{
+    int x{35};
+
+    //auto y = std::rotl(x); //invalid
+    //auto n = std::popcount(x); //invalid
+    //auto b = std::popcount(std::bitset<16>{3u}); //invalid
+}
